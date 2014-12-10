c4c72ea57e8553619512944d90ec7faaa1981928
charles peters <charlespeters42@gmail.com>
2014-12-10 13:05:18 -0500

    adding in a few animations and a shitty test

---
+++ Gulpfile.js
@@ -0,0 +1,41 @@
+var gulp     = require('gulp'),
+sass         = require('gulp-ruby-sass'),
+plumber      = require('gulp-plumber'),
+autoprefixer = require('gulp-autoprefixer'),
+browserSync  = require('browser-sync'),
+reload       = browserSync.reload,
+name         = 'mickey',
+src          = 'src/',
+dist         = 'test/';
+
+
+gulp.task('styles', function(){
+  gulp.src('*.scss')
+  .pipe(plumber())
+  .pipe(sass({style: 'compressed', "sourcemap=none": true }))
+  .pipe(autoprefixer('last 2 version'))
+  .pipe(gulp.dest(dist + 'css/'))
+  .pipe(reload({stream:true}));
+});
+
+
+gulp.task('html', function () {
+  gulp.src(dist+'*.html')
+  .pipe(reload({stream: true}));
+});
+
+gulp.task('browser-sync', function() {
+  browserSync({
+    server: {
+      baseDir: "./test"
+    }
+  });
+});
+
+
+gulp.task('watch', function(){
+  gulp.watch(['*.scss', '**/*.scss'], ['styles']);
+  gulp.watch(dist+'*.html', ['html']);
+});
+
+gulp.task('default', ['styles', 'watch', 'browser-sync']);
